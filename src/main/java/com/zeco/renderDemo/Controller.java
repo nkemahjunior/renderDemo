46bc7f8f8db1c154e4c9239af326f01f3333b9e1
Nkemah_Junior <nkemahjunior679205967@gmail.com>
2024-03-23 02:58:30 +0100

    initial commit

---
+++ src/main/java/com/zeco/renderDemo/Controller.java
@@ -0,0 +1,24 @@
+package com.zeco.renderDemo;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+    @GetMapping("/test1")
+    public String test1(){
+        return "deployment 1";
+    }
+
+ /*   @GetMapping("/test2")
+    public String test2(){
+        return "deployment 2";
+    }
+
+    @GetMapping("/test2")
+    public String test3(){
+        return "deployment 3";
+    }*/
+}
---
+++ src/test/java/com/zeco/renderDemo/RenderDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zeco.renderDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RenderDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
